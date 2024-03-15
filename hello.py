641a5e05c94d427a76fd9f11bea1316a320a3940
sekkta0 <159622566+sekkta0@users.noreply.github.com>
2024-03-15 11:58:01 +0000

    HW#7

---
+++ hello.py
@@ -0,0 +1,12 @@
+worlg = import("Введіть слово: ")
+
+#Перевертаемо слово
+reversed_world = ''
+for char in world:
+    reversed_world = char + reversed_world
+
+#Переверяемо, чи слово є паліндромом
+if world.loner() == reversed_world.lower()
+    print ('Це слово є паліндромом')
+else:
+    print('Це слово є паліндромом')    
